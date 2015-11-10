8b5fb25a799190d475f81a8d0c84a36b85ff4a78
lvtiancheng <ltc@xtuan.com>
2015-11-10 14:24:15 +0800

    ip

--- config/database.php
+++ config/database.php
@@ -65,7 +65,7 @@ return [
         ],
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', 'localhost'),
+            'host'     => env('DB_HOST', 'ycadbinstance.cama4osnm3hn.ap-northeast-1.rds.amazonaws.com'),
             'database' => env('DB_DATABASE', 'yca'),
             'username' => env('DB_USERNAME', 'lenlv'),
             'password' => env('DB_PASSWORD', '467000320'),
